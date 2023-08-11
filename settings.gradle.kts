dd8f4c39580f4b814ca37c4be0eaafa1eddc0e78
Silvio Giebl <silvio.giebl@hivemq.com>
2023-08-11 17:03:27 +0200

    Replace utf8 with defaults plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,8 @@ rootProject.name = "hivemq-google-cloud-pubsub-hello-world-customization"
 
 pluginManagement {
     plugins {
+        id("io.github.sgtsilvio.gradle.defaults") version "${extra["plugin.defaults.version"]}"
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-        id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
     }
 }
 
